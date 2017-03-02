27e05819fa668578613f039c32363d1db3908fb0
Kim Nguyen <design@kimisgold.com>
2017-03-02 12:43:05 -0500

    Restructure exhibit navigation to sidebar layout that follows the scroll. (#2)

--- exhibit-builder/exhibits/show.php
+++ exhibit-builder/exhibits/show.php
@@ -1,4 +1,5 @@
 <?php
+queue_js_file(array('thedaily-exhibits'), 'js');
 queue_js_string('
     jQuery("document").ready(function() {
         var levels = jQuery(".parent").length;
@@ -19,22 +20,6 @@ echo head(array(
 
 <div id="exhibit-blocks">
 <?php exhibit_builder_render_exhibit_page(); ?>
-</div>
-
-<div id="exhibit-page-navigation">
-    <?php if ($prevLink = exhibit_builder_link_to_previous_page()): ?>
-    <div id="exhibit-nav-prev">
-    <?php echo $prevLink; ?>
-    </div>
-    <?php endif; ?>
-    <?php if ($nextLink = exhibit_builder_link_to_next_page()): ?>
-    <div id="exhibit-nav-next">
-    <?php echo $nextLink; ?>
-    </div>
-    <?php endif; ?>
-    <div id="exhibit-nav-up">
-    <?php echo exhibit_builder_page_trail(); ?>
-    </div>
 </div>
 
 <?php echo foot(); ?>
---
+++ js/thedaily-exhibits.js
@@ -0,0 +1,59 @@
+(function($) {
+    $(document).ready(function() {
+
+        // Exhibit sidebar scrolling
+
+        var $sidebar   = $("#exhibit-pages"), 
+            $window    = $(window),
+            offset     = $sidebar.offset(),
+            topPadding = 15;
+
+        var sidebarScroller = function() {
+            $window.scroll(function() {
+                if ($window.scrollTop() > offset.top) {
+                    $sidebar.stop().animate({
+                        marginTop: $window.scrollTop() - offset.top + topPadding
+                    });
+                } else {
+                    $sidebar.stop().animate({
+                        marginTop: 0
+                    });
+                }
+            });
+        };
+
+        if ($window.width() > 640) {
+            sidebarScroller();
+        }
+
+        $window.resize(function() {
+            if ($(this).width() > 640) {
+                sidebarScroller();
+            }
+        });
+
+        // Exhibit sidebar submenu toggle management
+
+        $('#exhibit-pages li ul').each(function() {
+            var subpages = $(this)
+            var subpageToggle = '<button class="subpages-toggle collapsed" aria-label="Expand"></button>'
+            subpages.siblings('a').addClass('has-children');
+            subpages.before(subpageToggle);
+            $('.has-children').parent('li').addClass('collapsed');
+        });
+
+        $('li.parent, li.current').children('.subpages-toggle').attr('class', 'subpages-toggle expanded').attr('aria-label', 'Collapse');
+        $('li.parent.collapsed, li.current.collapsed').removeClass('collapsed').addClass('expanded');
+
+        $('#exhibit-pages').on('click', '.subpages-toggle', function() {
+            var subpageToggle = $(this);
+            subpageToggle.toggleClass('collapsed').toggleClass('expanded');
+            subpageToggle.parent('li').toggleClass('collapsed').toggleClass('expanded');
+            if (subpageToggle.hasClass('collapsed')) {
+                subpageToggle.attr('aria-label', 'Expand');
+            } else {
+                subpageToggle.attr('aria-label', 'Collapsed');
+            }
+        });
+    });
+})(jQuery)
