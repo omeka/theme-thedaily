010736b927d72e2860933961204a427aa759b366
Kim Nguyen <design@kimisgold.com>
2020-09-11 21:13:24 -0400

    Remove alt text check. (omeka/Omeka#925)

--- common/header.php
+++ common/header.php
@@ -63,8 +63,7 @@
 
         </header>
 
-        <?php $altText = (get_theme_option('header_image_alt') !== null) ? get_theme_option('header_image_alt') : ''; ?>
-        <?php echo theme_header_image($altText); ?>
+          <?php echo theme_header_image(); ?>
 
         <article id="content" role="main">
 
