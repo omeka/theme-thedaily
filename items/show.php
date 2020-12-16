5856db305a59752d944e13889b510a21154377f7
kalbers <ken.albers@gmail.com>
2020-12-16 14:43:04 -0500

    Update for rich titles

--- items/show.php
+++ items/show.php
@@ -40,7 +40,7 @@ echo head(array('title' => metadata('item', array('Dublin Core', 'Title')), 'bod
             <?php echo file_image('original', array(), $image); ?>
             </div>
             <div class="media-link-<?php echo $imageCount; ?>">
-            <a href="<?php echo $fileUrl; ?>"><?php echo metadata($image, 'display_title'); ?></a>
+            <a href="<?php echo $fileUrl; ?>"><?php echo metadata($image, 'rich_title', array('no_escape' => true)); ?></a>
             </div>
         </li>
         <?php endforeach; ?>
